5ca9cd2976d160048907eb18cd2a325b9601ca0d
Vahe <vahe-sargsyan-2021@mail.ru>
2025-02-13 19:27:11 +0400

    Initial Commit 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,7 @@ android {
     namespace = "com.project.kuk"
     compileSdk = 35
 
+
     defaultConfig {
         applicationId = "com.project.kuk"
         minSdk = 28
@@ -29,6 +30,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+        buildFeatures {
+            viewBinding = true
+    }
 }
 
 dependencies {
@@ -37,6 +41,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/project/kuk/MainMenuActivity.java
+++ app/src/main/java/com/project/kuk/MainMenuActivity.java
@@ -1,24 +1,51 @@
 package com.project.kuk;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainMenuActivity extends AppCompatActivity {
 
-    @Override
+    BottomNavigationView bottomNavigationView;
+    HomeFragment homeFragment = new HomeFragment();
+    CartFragment cartFragment = new CartFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main_menu);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected( MenuItem item) {
+
+                    int itemId = item.getItemId();
+
+                    if (itemId == R.id.home) {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+                        return true;
+                    } else if (itemId == R.id.cart) {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,cartFragment).commit();
+                        return true;
+                    } else if (itemId == R.id.profile) {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,profileFragment).commit();
+                        return true;
+                    }
+
+                return false;
+            }
         });
+
     }
 }

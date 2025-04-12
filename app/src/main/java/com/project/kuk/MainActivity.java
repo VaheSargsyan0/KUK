d461484e8cc0da8f8f55f6543a7db73d97ce8ec8
Vahe <vahe-sargsyan-2021@mail.ru>
2025-04-12 21:32:03 +0400

    Initial Commit 8

--- app/src/main/java/com/project/kuk/MainActivity.java
+++ app/src/main/java/com/project/kuk/MainActivity.java
@@ -1,115 +1,69 @@
 package com.project.kuk;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.PhoneAuthCredential;
-import com.google.firebase.auth.PhoneAuthOptions;
-import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.database.FirebaseDatabase;
+import com.project.kuk.databinding.ActivityMainBinding;
 
-import java.util.concurrent.TimeUnit;
+import java.util.HashMap;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private boolean otpSent = false;
-    private String id = "";
-
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        final EditText mobileET = findViewById(R.id.mobileET);
-        final EditText otp = findViewById(R.id.otp);
-        final Button send_sms = findViewById(R.id.send_sms);
-
-        FirebaseApp.initializeApp(this);
-
-        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-
-        send_sms.setOnClickListener(new View.OnClickListener() {
+        binding.signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if (otpSent) {
-                    final String getOTP = otp.getText().toString();
-
-                    if (id.isEmpty()) {
-                        Toast.makeText(MainActivity.this, "Unable to verify", Toast.LENGTH_SHORT).show();
-                    } else {
-
-                        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(id, getOTP);
-
-                        firebaseAuth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-
-                                if (task.isSuccessful()) {
-
-                                    FirebaseUser userDetails = task.getResult().getUser();
-
-                                    Toast.makeText(MainActivity.this, "Verified", Toast.LENGTH_SHORT).show();
-                                } else {
-                                    Toast.makeText(MainActivity.this, "Something went wrong", Toast.LENGTH_LONG).show();
-                                }
-                            }
-                        });
-                    }
-
-
-                }  else {
-                    final String getMobile = mobileET.getText().toString();
-
-                    PhoneAuthOptions options = PhoneAuthOptions.newBuilder(firebaseAuth)
-                            .setPhoneNumber(getMobile)
-                            .setTimeout(60L, TimeUnit.SECONDS)
-                            .setActivity(MainActivity.this)
-                            .setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            public void onClick(View v) {
+                if (binding.emailEt.getText().toString().isEmpty() || binding.passwordEt.getText().toString().isEmpty()
+                        || binding.usernameEt.getText().toString().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Fields cannot be empty", Toast.LENGTH_SHORT).show();
+                } else {
+                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(binding.emailEt.getText().toString(), binding.passwordEt.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                                 @Override
-                                public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        HashMap<String, String> userInfo = new HashMap<>();
+                                        userInfo.put("email", binding.emailEt.getText().toString());
+                                        userInfo.put("username", binding.usernameEt.getText().toString());
 
-                                    Toast.makeText(MainActivity.this, "Code sent successfully", Toast.LENGTH_SHORT).show();
+                                        FirebaseDatabase.getInstance().getReference().child("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                                .setValue(userInfo);
+
+                                        startActivity(new Intent(MainActivity.this, MainMenuActivity.class));
+                                    } else {
+                                        Toast.makeText(MainActivity.this, "Registration failed: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                                    }
                                 }
-
-                                @Override
-                                public void onVerificationFailed(@NonNull FirebaseException e) {
-
-                                    Toast.makeText(MainActivity.this, "Something went wrong " +e.getMessage(), Toast.LENGTH_LONG).show();
-                                }
-
-                                @Override
-                                public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
-                                    super.onCodeSent(s, forceResendingToken);
-
-                                    otp.setVisibility(View.VISIBLE);
-                                    send_sms.setText("Verify OTP");
-                                    id = s;
-                                    otpSent = true;
-                                }
-                            }).build();
-
-                    PhoneAuthProvider.verifyPhoneNumber(options);
+                            });
                 }
-
             }
         });
 
+        binding.backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SignInActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/project/kuk/SignInActivity.java
+++ app/src/main/java/com/project/kuk/SignInActivity.java
@@ -3,30 +3,53 @@ package com.project.kuk;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.project.kuk.databinding.ActivitySignInBinding;
+
 
 public class SignInActivity extends AppCompatActivity {
+
+    private ActivitySignInBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_sign_in);
-        Button goToSignUp = (Button) findViewById(R.id.btn_to_signup);
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        View.OnClickListener oclGoToSignUp =  new View.OnClickListener() {
+        binding.loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(SignInActivity.this, MainActivity.class);
-                startActivity(intent);
+            public void onClick(View v) {
+                if (binding.emailEt.getText().toString().isEmpty() || binding.passwordEt.getText().toString().isEmpty()){
+                    Toast.makeText(getApplicationContext(), "Fields cannot be empty", Toast.LENGTH_SHORT).show();
+                }else{
+                    FirebaseAuth.getInstance().signInWithEmailAndPassword(binding.emailEt.getText().toString(), binding.passwordEt.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()){
+                                        startActivity(new Intent(SignInActivity.this, MainMenuActivity.class));
+                                    }
+                                }
+                            });
+                }
             }
-        };
-        goToSignUp.setOnClickListener(oclGoToSignUp);
+        });
+
+        binding.goToRegisterActivityTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignInActivity.this, MainActivity.class));
+            }
+        });
     }
 }
